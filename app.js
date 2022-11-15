14112fbef8919a472ca839c88d5fd0dcbebb4ce6
Mallikarjun Neela <117260058+mallikarjunneela@users.noreply.github.com>
2022-11-15 11:41:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,65 @@ const convertStateDbObjectToResponseObject = (dbObject) => {
     gender: dbObject.gender,
   };
 };
+
+app.post("/register/", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+  const postRegisterQuery = `SELECT * FROM user WHERE username = '${username}';`;
+  const databaseUser = await database.get(postRegisterQuery);
+  if (databaseUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, database.password);
+    if (isPasswordMatched === true) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const databasePath = path.join(__dirname, "twitterClone.db");
+const app = express();
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () =>
+      console.log("Server is Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+const convertStateDbObjectToResponseObject = (dbObject) => {
+  return {
+    username: dbObject.username,
+    password: dbObject.password,
+    name: dbObject.name,
+    gender: dbObject.gender,
+  };
+};
 // API 1
 app.post("/register/", async (request, response) => {
   const { username, password, name, gender } = request.body;
@@ -66,31 +125,27 @@ app.post("/register/", async (request, response) => {
 // API 2
 app.post("/login/", async (request, response) => {
   const { username, password } = request.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
 
-  const postLoginQuery = `SELECT * FROM user WHERE username = '${username}'`;
-  const databaseUser = await database.get(postLoginQuery);
+  const checkUser = `SELECT * FROM user WHERE username = '${username}'`;
+  const databaseUser = await database.get(checkUser);
 
-  if (databaseUser === undefined) {
-    response.status(400);
-    response.send("Invalid user");
-  } else {
+  if (databaseUser !== undefined) {
       const isPasswordMatched = await bcrypt.compare(
       password,
-      databaseUser.password
+      databaseUser.password);
+      if (passwordCheck === false){
+          response.status(400);
+          response.send("Invalid password");
 
-    if (isPasswordMatched === true) {
-      response.status(400);
-      response.send("Invalid password");
-    } else {
-      const loginNewUser = `INSERT INTO user (username, password) 
-            VALUES ('${username}', '${hashedPassword}')`;
-
-      await database.run(LoginNewUser);
-      response.status(200);
-      response.send({ jwtToken });
+    }  else {
+        const payload = {username: username}
+        const jwtToken = jwt.sign(payload, "SECRET_KEY");
+        response.send({jwtToken: jwtToken})
     }
-  }
+    else {
+        response.status(400)
+        response.send("Invalid user")
+    }
 });
 
 // Authenticate with JWT Token
